7958840ee937042d7fcd914574be3bf8d62cadde
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-02-12 19:17:38 +0100

    Improve styling of the topbar

--- components/topbar.tsx
+++ components/topbar.tsx
@@ -3,18 +3,13 @@ import { Button } from './ui/button';
 
 export default function Topbar() {
   return (
-    <header className='absolute flex items-center justify-between gap-8 px-4 h-12 w-full backdrop-brightness-75 border-b'>
-      <span className='text-white'>Rhova</span>
+    <header className='z-30 absolute flex items-center justify-between gap-8 px-4 h-12 w-full backdrop-brightness-250'>
+      <span className=''>Rhova</span>
       <div className='flex gap-2'>
-        <Button
-          variant='ghost'
-          className='text-white font-bold cursor-pointer h-8'
-        >
+        <Button variant='ghost' className='font-bold cursor-pointer h-8'>
           Start a quiz
         </Button>
-        <Button className='bg-white text-black font-bold cursor-pointer h-8'>
-          Create a quiz
-        </Button>
+        <Button className='font-bold cursor-pointer h-8'>Create a quiz</Button>
       </div>
     </header>
   );
