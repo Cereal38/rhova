63e97d0c5d27ba3916673e60683dc6163c3558f0
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-02-14 08:52:31 +0100

    Add buttons and init step management for the form

--- app/quiz-settings/create/page.tsx
+++ app/quiz-settings/create/page.tsx
@@ -4,17 +4,26 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
 import CreateQuizFieldGroup from './create-quiz-field-group';
-import { useActionState } from 'react';
+import { useActionState, useState } from 'react';
 import { createQuiz } from '@/lib/actions/create-quiz-actions';
 import Form from 'next/form';
 import AddQuestionFieldGroup from './add-question-field-group';
+import { Button } from '@/components/ui/button';
+
+const enum Steps {
+  'BASIC_INFO',
+  'ADD_QUESTION',
+}
 
 export default function CreateQuizPage() {
   const [actionState, formAction] = useActionState(createQuiz, null);
+  const [currentStep, setCurrentStep] = useState<Steps>(Steps.BASIC_INFO);
+
   return (
     <main>
       <Card>
@@ -26,11 +35,39 @@ export default function CreateQuizPage() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <Form action={formAction} className='flex flex-col gap-8'>
+          <Form
+            id='create-quiz'
+            action={formAction}
+            className='flex flex-col gap-8'
+          >
             <CreateQuizFieldGroup />
-            <AddQuestionFieldGroup />
+            {/* <AddQuestionFieldGroup /> */}
           </Form>
         </CardContent>
+        <CardFooter>
+          <div className='flex justify-between w-full'>
+            {currentStep !== Steps.BASIC_INFO && (
+              <Button type='button' variant='secondary'>
+                Back
+              </Button>
+            )}
+            <div className='flex gap-4'>
+              <Button
+                type='button'
+                variant={
+                  currentStep === Steps.BASIC_INFO ? 'default' : 'secondary'
+                }
+              >
+                Add a question
+              </Button>
+              {currentStep === Steps.ADD_QUESTION && (
+                <Button form='create-quiz' type='submit'>
+                  Create
+                </Button>
+              )}
+            </div>
+          </div>
+        </CardFooter>
       </Card>
     </main>
   );
