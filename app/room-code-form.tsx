985b64ac18c306af3e35dac43575776c39a57fbe
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-02-17 21:44:40 +0100

    Add some test code for the websocket

--- app/room-code-form.tsx
+++ app/room-code-form.tsx
@@ -1,7 +1,7 @@
 import { Field } from '@/components/ui/field';
 import Form from 'next/form';
 import { submitRoomCode } from '@/lib/actions/room-code-action';
-import { useActionState } from 'react';
+import { useActionState, useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useSocket } from '@/hooks/use-socket';
@@ -10,6 +10,43 @@ export default function RoomCodeForm() {
   const [actionState, formAction] = useActionState(submitRoomCode, null);
   // TODO: Remove this test — temporary socket connection verification
   const { socket, isConnected } = useSocket();
+  const [roomCode, setRoomCode] = useState<string | null>(null);
+  const [roomCodeInput, setRoomCodeInput] = useState<string>('');
+
+  // TODO: Remove this. It is for test purpose
+  const handleCreateRoom = () => {
+    console.log('Handle create room triggered...');
+    if (!socket || !isConnected) {
+      console.log('Create room failed');
+      return;
+    }
+
+    socket.emit(
+      'create-session',
+      { title: 'Test quiz', questions: [] },
+      (res: { roomCode: string }) => {
+        console.log('Session created: ', res.roomCode);
+        setRoomCode(res.roomCode);
+      },
+    );
+  };
+
+  const handleJoinRoom = () => {
+    console.log('Trying to join a room...');
+
+    if (!socket || !isConnected) {
+      console.log('Joining room failed');
+      return;
+    }
+
+    socket.emit(
+      'join-session',
+      roomCodeInput,
+      (res: { success: boolean; error?: string }) => {
+        console.log('Join success: ', res.success);
+      },
+    );
+  };
 
   return (
     <Form action={formAction} className='flex flex-col gap-4'>
@@ -18,6 +55,30 @@ export default function RoomCodeForm() {
         Socket: {isConnected ? '🟢 Connected' : '🔴 Disconnected'}
         {socket?.id && ` (${socket.id})`}
       </p>
+      <Button type='button' onClick={handleCreateRoom} disabled={!isConnected}>
+        Create Session
+      </Button>
+      <span>{`Room code: ${roomCode}`}</span>
+      <Input
+        type='text'
+        inputMode='text'
+        autoCapitalize='characters'
+        autoCorrect='off'
+        spellCheck={false}
+        maxLength={6}
+        placeholder='Enter room code'
+        className='h-16 text-center font-mono tracking-[0.25em] uppercase'
+        value={roomCodeInput}
+        onChange={(e) => {
+          e.currentTarget.value = e.currentTarget.value
+            .toUpperCase()
+            .replace(/[^A-Z0-9]/g, '');
+          setRoomCodeInput(e.currentTarget.value);
+        }}
+      />
+      <Button type='button' onClick={handleJoinRoom} disabled={!isConnected}>
+        Join Session
+      </Button>
       <Field>
         <Input
           type='text'
