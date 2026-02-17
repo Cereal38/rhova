79d25f962fefb9ab5730268a1c4bbe0f23d8ec07
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-02-17 22:09:23 +0100

    The player counter is now working

--- app/quiz/[id]/host-waiting-room/player-counter.tsx
+++ app/quiz/[id]/host-waiting-room/player-counter.tsx
@@ -1,8 +1,21 @@
 'use client';
 
-import { useState } from 'react';
+import { useSocket } from '@/hooks/use-socket';
+import { useEffect, useState } from 'react';
 
 export default function PlayerCounter() {
+  const { socket } = useSocket();
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on('player-count', (data) => setPlayerCount(data.count));
+
+    return () => {
+      socket.off('player-count', (data) => setPlayerCount(data.count));
+    };
+  }, [socket]);
+
   const [playerCount, setPlayerCount] = useState(0);
 
   return <p>{playerCount} players ready!</p>;
