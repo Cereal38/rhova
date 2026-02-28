ae6214d7e3f0fbf5d92e8dc302f23793be29d74b
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-02-28 10:32:41 +0100

    Get and display the question number on the player question page

--- app/quiz/[roomCode]/player/question/page.tsx
+++ app/quiz/[roomCode]/player/question/page.tsx
@@ -1,7 +1,39 @@
+'use client';
+
+import { useSocket } from '@/hooks/use-socket';
+import WsCallback from '@/models/ws-callback';
+import WsQuestion from '@/models/ws-question';
+import { notFound, useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 export default function PlayerQuestionPage() {
+  const { roomCode } = useParams();
+  const { socket } = useSocket();
+
+  const [question, setQuestion] = useState<WsQuestion>();
+  const [roomNotFound, setRoomNotFound] = useState(false);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const getQuestionHandler = (res: WsCallback<WsQuestion>) => {
+      if (!res.success || !res.payload) {
+        setRoomNotFound(true);
+      }
+
+      setQuestion(res.payload);
+    };
+
+    socket.emit('get-question', roomCode, getQuestionHandler);
+  }, [socket, roomCode]);
+
+  if (roomNotFound) {
+    notFound();
+  }
+
   return (
-    <main>
-      <h1>Player question page</h1>
+    <main className='h-full'>
+      {question && <h1>Question {question.questionIndex + 1}</h1>}
     </main>
   );
 }
