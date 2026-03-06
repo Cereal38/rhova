aa3061050cf3fdab8d9e7d2b04b214644a4d2984
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-06 19:55:18 +0100

    Add QuizFinishedStepContent component to display the final score after quiz completion

---
+++ app/quiz/[roomCode]/player/question/quiz-finished-step-content.tsx
@@ -0,0 +1,20 @@
+import { WsPlayerScore } from '@/models/ws-player-score';
+
+interface Props {
+  playerFinalScore: WsPlayerScore;
+}
+
+export default function QuizFinishedStepContent({ playerFinalScore }: Props) {
+  return (
+    <main className='h-full flex flex-col p-8'>
+      <h1 className='flex-1 flex items-center justify-center text-5xl'>
+        Quiz finished!
+      </h1>
+      <div className='flex flex-col gap-4 items-center'>
+        <span className='text-xl'>
+          {playerFinalScore.score}/{playerFinalScore.total}
+        </span>
+      </div>
+    </main>
+  );
+}
