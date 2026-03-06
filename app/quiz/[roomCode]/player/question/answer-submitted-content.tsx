f753752f24cb3815415ddbe42c33b2505b7936b9
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-06 19:43:40 +0100

    Add AnswerSubmittedContent component to display confirmation after answer submission

---
+++ app/quiz/[roomCode]/player/question/answer-submitted-content.tsx
@@ -0,0 +1,21 @@
+import AnswerButton from '@/components/answer-button';
+
+interface Props {
+  answerNumber: number;
+}
+
+export default function AnswerSubmittedContent({ answerNumber }: Props) {
+  return (
+    <main className='h-full flex flex-col p-8'>
+      <h1 className='flex-1 flex items-center justify-center text-5xl'>
+        Answer submitted
+      </h1>
+      <div className='flex flex-col gap-4 items-center'>
+        <AnswerButton number={answerNumber} iconOnly={true} />
+        <span className='opacity-75 text-center'>
+          Your answer has been registered. Waiting for the host.
+        </span>
+      </div>
+    </main>
+  );
+}
