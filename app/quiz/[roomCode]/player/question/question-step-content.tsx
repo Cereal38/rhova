84e0baeb1d46e43f2f64bd90b685894392f82f32
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-06 19:40:15 +0100

    Create the question-step-content componetn

---
+++ app/quiz/[roomCode]/player/question/question-step-content.tsx
@@ -0,0 +1,35 @@
+import AnswerButton from '@/components/answer-button';
+import WsQuestion from '@/models/ws-question';
+
+interface Props {
+  question: WsQuestion;
+  onSubmitAnswer: () => void;
+}
+
+export default function QuestionStepContent({
+  question,
+  onSubmitAnswer,
+}: Props) {
+  return (
+    <main className='h-full flex flex-col p-8'>
+      {question && (
+        <>
+          <h1 className='flex-1 flex items-center justify-center text-5xl'>
+            Question {question.questionIndex + 1}
+          </h1>
+          <div className='grid grid-cols-2 gap-4'>
+            {question.answers.map((answer, index) => (
+              <AnswerButton
+                key={answer}
+                number={index + 1}
+                iconOnly={true}
+                clickable={true}
+                onClick={onSubmitAnswer}
+              />
+            ))}
+          </div>
+        </>
+      )}
+    </main>
+  );
+}
