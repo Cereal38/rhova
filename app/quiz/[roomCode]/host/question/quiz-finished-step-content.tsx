9f3f4c2a3653a128cbe4ebb3a47a64c4cc562256
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-07 08:17:59 +0100

    Create a simple quiz finish content component which would display the average score of players on the host screen

---
+++ app/quiz/[roomCode]/host/question/quiz-finished-step-content.tsx
@@ -0,0 +1,33 @@
+'useClient';
+
+import WsLeaderboardItem from '@/models/ws-leaderboard-item';
+import { useEffect, useState } from 'react';
+
+interface Props {
+  leaderboard: WsLeaderboardItem[];
+}
+
+export default function QuizFinishedStepContent({ leaderboard }: Props) {
+  const [averageScore, setAverageScore] = useState<number>();
+
+  useEffect(() => {
+    const totalScore = leaderboard.reduce(
+      (acc, player) => acc + player.score,
+      0,
+    );
+    const numberOfPlayers = leaderboard.length;
+
+    setAverageScore(totalScore / numberOfPlayers);
+  }, [leaderboard]);
+
+  return (
+    <main className='h-full mx-auto w-[90%] overflow-y-auto py-12'>
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-2xl font-bold'>Quiz finished!</h1>
+        <p className='text-sm text-gray-500'>
+          The quiz has finished. The average score is {averageScore}.
+        </p>
+      </div>
+    </main>
+  );
+}
