d66400fc20c7217d45591e8104decb49849b513a
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-14 08:54:12 +0100

    Improve the player question screen so answer buttons now take the full height of the screen

--- app/quiz/[roomCode]/player/question/question-step-content.tsx
+++ app/quiz/[roomCode]/player/question/question-step-content.tsx
@@ -11,24 +11,19 @@ export default function QuestionStepContent({
   onSubmitAnswer,
 }: Props) {
   return (
-    <main className='h-full flex flex-col p-8'>
+    <main className='h-full flex flex-col'>
       {question && (
-        <>
-          <h1 className='flex-1 flex items-center justify-center text-5xl'>
-            Question {question.questionIndex + 1}
-          </h1>
-          <div className='grid grid-cols-2 gap-4'>
-            {question.answers.map((answer, index) => (
-              <AnswerButton
-                key={answer}
-                number={index + 1}
-                iconOnly={true}
-                clickable={true}
-                onClick={() => onSubmitAnswer(answer, index + 1)}
-              />
-            ))}
-          </div>
-        </>
+        <div className='grid grid-cols-2 gap-4 flex-1 p-4'>
+          {question.answers.map((answer, index) => (
+            <AnswerButton
+              key={answer}
+              number={index + 1}
+              iconOnly={true}
+              clickable={true}
+              onClick={() => onSubmitAnswer(answer, index + 1)}
+            />
+          ))}
+        </div>
       )}
     </main>
   );
