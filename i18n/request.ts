f06ebf2918d5ae00c1f7a1f0db7d427d4cb051f7
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-15 17:53:55 +0100

    Update intl config so it now use the user browser dfault language

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,8 +1,17 @@
 import { getRequestConfig } from 'next-intl/server';
+import { cookies, headers } from 'next/headers';
 
 export default getRequestConfig(async () => {
-  // TODO: Should not be static
-  const locale = 'en';
+  // If lang is set in cookies, use it, else get browser default
+  // and save it to cookies
+  const store = await cookies();
+  let locale: string | undefined = store.get('locale')?.value;
+
+  if (!locale) {
+    const headersList = await headers();
+    const acceptLanguage = headersList.get('accept-language') ?? '';
+    locale = acceptLanguage.includes('fr') ? 'fr' : 'en';
+  }
 
   return {
     locale,
