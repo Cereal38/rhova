3bc0de07deef5a807789ae83548031704c3a8019
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-19 20:51:29 +0100

    Add the logo to the topbar

--- components/clickable-logo.tsx
+++ components/clickable-logo.tsx
@@ -1,9 +1,20 @@
 import { routes } from '@/lib/routes';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function ClickableLogo() {
   return (
-    <Link href={routes.home()} className='text-white font-bold'>
+    <Link
+      href={routes.home()}
+      className='text-white font-bold flex items-center gap-2'
+    >
+      <Image
+        src='/logo.svg'
+        alt=''
+        width={24}
+        height={24}
+        className='brightness-0 invert'
+      />
       Rhova
     </Link>
   );
