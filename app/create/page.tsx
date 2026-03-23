3e18ca4f5e8e337e4ead3b3bbb8383a3fdd026b9
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-23 20:49:33 +0100

    Populate the create a quiz with some code

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,3 +1,16 @@
-export default function CreateQuiz() {
-  return <main>Create a quiz page</main>;
+import { Card } from '@/components/ui/card';
+import { getTranslations } from 'next-intl/server';
+
+export default async function CreateQuiz() {
+  const t = await getTranslations();
+
+  return (
+    <main className='flex-1 flex items-center justify-center max-w-[600px] m-auto '>
+      <Card className='flex flex-col gap-2 p-8 md:min-w-[400px]'>
+        <h1 id='create-quiz-title' className='text-xl'>
+          {t('create-quiz.title')}
+        </h1>
+      </Card>
+    </main>
+  );
 }
