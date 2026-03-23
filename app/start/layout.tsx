7bb11af76db00d95bbbcbd212e8dd1783ec3890c
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-23 20:15:50 +0100

    The start quiz page now also uses the Topbar component. This fixed a glitch

--- app/start/layout.tsx
+++ app/start/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next';
-import StartQuizTopbar from './start-quiz-topbar';
 import { getTranslations } from 'next-intl/server';
 import GradientBackground from '@/components/gradient-background';
+import { TopbarItem } from '@/models/interfaces/topbar-item';
+import { routes } from '@/lib/routes';
+import Topbar from '@/components/topbar';
 
 export async function generateMetadata(): Promise<Metadata> {
   const t = await getTranslations();
@@ -11,15 +13,24 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default function StartQuizLayout({
+export default async function StartQuizLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const t = await getTranslations();
+
+  const topbarItems: TopbarItem[] = [
+    {
+      label: t('common.join'),
+      href: routes.home(),
+    },
+  ];
+
   return (
     <div className='relative min-h-dvh flex flex-col'>
       <GradientBackground />
-      <StartQuizTopbar />
+      <Topbar items={topbarItems} />;
       <div className='flex-1 flex flex-col'>{children}</div>
     </div>
   );
