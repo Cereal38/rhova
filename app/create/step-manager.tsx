21380da1339e9364089957982455c463263fd2c6
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-03-25 10:23:31 +0100

    Now when the step change when craeting a quiz the new step is saved in query params

--- app/create/step-manager.tsx
+++ app/create/step-manager.tsx
@@ -1,15 +1,28 @@
 'use client';
 
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import SelectModStep from './select-mod-step';
 import { CreateQuizStep } from '@/models/enums/create-quiz-step';
 import CreateSetTitleStep from './create-set-title-step';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 export default function StepManager() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
   const [step, setStep] = useState<CreateQuizStep>(CreateQuizStep.SelectMod);
+
+  const createQueryString = (name: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(name, value);
+
+    return params.toString();
+  };
 
   function stepChangeHandler(step: CreateQuizStep) {
     setStep(step);
+    router.push(pathname + '?' + createQueryString('step', step));
   }
 
   switch (step) {
