acc485bd9f7dff497a8be29ca6574fb903645bef
Cereal38 <guillaume.bruncosmegazot@gmail.com>
2026-04-12 09:08:02 +0200

    Add a layout for the contact page

---
+++ app/contact/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
+import GradientBackground from '@/components/gradient-background';
+import { TopbarItem } from '@/models/interfaces/topbar-item';
+import { routes } from '@/lib/routes';
+import Topbar from '@/components/topbar';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations();
+  return {
+    title: 'Rhova Contact Page',
+    description: t('metadata.contact-description'),
+  };
+}
+
+export default async function ContactLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const t = await getTranslations();
+
+  const topbarItems: TopbarItem[] = [
+    {
+      label: t('common.create-a-quiz'),
+      href: routes.start(),
+      variant: 'ghost',
+    },
+    {
+      label: t('common.start-a-quiz'),
+      href: routes.start(),
+      variant: 'ghost',
+    },
+    {
+      label: t('common.join'),
+      href: routes.home(),
+    },
+  ];
+
+  return (
+    <div className='relative min-h-dvh flex flex-col'>
+      <GradientBackground />
+      <Topbar items={topbarItems} />;
+      <div className='flex-1 flex flex-col pt-(--topbar-height) pb-4'>
+        {children}
+      </div>
+    </div>
+  );
+}
